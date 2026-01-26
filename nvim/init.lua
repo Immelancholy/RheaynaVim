7ff733f63815c6704076f76268c00ed7d9af6859
Immelancholy <lenalowes0@gmail.com>
2026-01-26 20:47:48 +0000

    refactor: configure tiny-inline-diagnostic options
    Updates the setup for tiny-inline-diagnostic to enable
    multiline support, show more detailed diagnostic info,
    and display the source of errors.

--- nvim/init.lua
+++ nvim/init.lua
@@ -395,7 +395,20 @@ nixInfo.lze.load({
 		auto_enable = true,
 		event = "VimEnter",
 		after = function(plugin)
-			require("tiny-inline-diagnostic").setup()
+			require("tiny-inline-diagnostic").setup({
+				options = {
+					add_messages = {
+						display_count = true,
+					},
+					multilines = {
+						enabled = true,
+						always_show = true,
+					},
+					show_source = {
+						enabled = true,
+					},
+				},
+			})
 			vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
 		end,
 	},
