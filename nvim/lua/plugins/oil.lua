2cc2f592121ca85db5fb4999e0257b8f2fccf03a
Immelancholy <lenalowes0@gmail.com>
2026-06-02 16:52:07 +0100

    refactor(oil): remove custom keymap from setup
    Revert to default configuration for oil.nvim keymaps.

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,11 +3,7 @@ return {
 	auto_enable = true,
 	lazy = false,
 	after = function()
-		require("oil").setup({
-			keymaps = {
-				["<C-w>"] = "actions.select",
-			},
-		})
+		require("oil").setup()
 		vim.keymap.set("n", "<leader>e", function()
 			vim.cmd("Oil")
 		end, { desc = "Open Oil" })
